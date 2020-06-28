99282550db23e7dc1c7f9363ffc15257afdb3d5a
Nathalie Rud <nathalier@gmail.com>
2020-06-28 02:23:36 +0100

    improvements

--- sponsor_parser.py
+++ sponsor_parser.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from glob import glob
 import pandas as pd
 import os
+from pathlib import Path
 import re
 import requests
 from sqlalchemy import create_engine
@@ -9,34 +10,10 @@ import sys
 from connection_settings import params
 
 
-def find_sponsors_url(source_url):
-    r = requests.get(source_url)
-    soup = BeautifulSoup(r.content, features="lxml")
-    sponsors_link = soup.find('section', {'id': 'documents'}). \
-        find('div', {'class': 'attachment-details'}).find('a').get('href')
-    return sponsors_link
-
-
-def get_sponsors_parsed(pdf_url):
-    doc_date = re.search('\/20(\d{2}-\d{2}-\d{2})', pdf_url).groups()[0].replace('-', '')
-    # TODO create 'sponsors' dir if not exists
-    base_dir = 'sponsors'
-    file_name_prefix = 'tier-2-5_sponsors'
-    with open(f'./{base_dir}/{file_name_prefix}_{doc_date}.pdf', 'wb') as f:
-        f.write(requests.get(pdf_url).content)
-    with open(f'./{base_dir}/{file_name_prefix}_{doc_date}.pdf', 'rb') as in_f,\
-         open(f'./{base_dir}/{file_name_prefix}_{doc_date}.xml', 'w', encoding='utf-8') as out_f:
-        # options: -q : quiet, -i : ignore images
-        cmd = f'pdftohtml -xml -enc UTF-8 -q -i {in_f.name} {out_f.name}'
-        os.system(cmd)
-    return f'./{base_dir}/{file_name_prefix}_{doc_date}.xml', doc_date
-
-
-def freshest_data():
-    # find all sponsors file and return the path to the freshest one
-    files = glob('./sponsors/tier*.csv')
-    files = sorted(files)
-    return files[-1]
+gov_url = r'https://www.gov.uk/government/publications/register-of-licensed-sponsors-workers'
+rel_data_path = './sponsors'
+Path(rel_data_path).mkdir(parents=True, exist_ok=True)
+file_name_prefix = 'tier-2-5_sponsors'
 
 
 class SponsorsData:
@@ -49,44 +26,33 @@ class SponsorsData:
     tier_subtypes = set([x[1] for x in tier_type_subtypes if str(x[1]) != 'nan'])
     counties = set(pd.read_csv('uk-counties-list.csv', header=None)[1])
 
-    def __init__(self, file_path:str, date:str=None, encoding='utf-8', to_csv=True,
-                 to_db=True, check_errors=True, to_db_if_error=False, db_settings=None):
-        if db_settings is None:
-            to_db = False
-        else:
-            self.db_params = db_settings
+    def __init__(self, file_path:str, date:str=None, encoding='utf-8'):
         self.file_encoding = encoding
         self.date = date or self._parse_date(file_path)
         if file_path.endswith('.xml'):
             self.xml_data_file = file_path
             self.csv_data_file = f'{self.xml_data_file[:-4]}.csv'
             self.sponsors_df = self._xml_to_df()
+            self.correct()
+            self._write_df_to_csv()
         elif file_path.endswith('.csv'):
             self.csv_data_file = file_path
             self.sponsors_df = pd.read_csv(self.csv_data_file, encoding=encoding)
         else:
             raise ValueError(f'Incorrect file format: {file_path}. '
                              f'xml or csv file is expected')
-        self.correct_df()
-        if to_csv:
-            self._write_df_to_csv()
-        # errors in data probable
-        self.prob_error = False
-        if check_errors:
-            self.check_df()
-        if to_db and ((not self.prob_error) or to_db_if_error):
-            self.insert_into_db()
 
     @staticmethod
     def _parse_date(fname):
         return re.search('(\d{6})', fname).groups()[0]
 
     def _write_df_to_csv(self):
-        self.sponsors_df.to_csv(self.csv_data_file, encoding='utf-8', index=False)
+        self.sponsors_df.to_csv(self.csv_data_file, encoding=self.file_encoding, index=False)
+        print('Sponsors file is successfully saved to CSV')
 
-    def insert_into_db(self):
-        conn_params = f"postgresql://{self.db_params['user']}:{self.db_params['password']}" \
-                      f"@{self.db_params['host']}/{self.db_params['database']}"
+    def insert_into_db(self, db_params=params):
+        conn_params = f"postgresql://{db_params['user']}:{db_params['password']}" \
+                      f"@{db_params['host']}/{db_params['database']}"
         engine = create_engine(conn_params)
 
         # insert tier types into db
@@ -112,35 +78,20 @@ class SponsorsData:
         with engine.connect() as con:
             con.execute('GRANT SELECT ON ALL TABLES IN SCHEMA public TO guest;')
 
-    def correct_df(self):
+    def correct(self):
+        # 1. In case of missed tier type calculate it from the subtype whenever possible
         if self.sponsors_df.loc[pd.isnull(self.sponsors_df['tier_type'])].size > 0:
             self.fix_missed_tier_type()
 
-    def check_df(self):
+    def validate(self):
+        no_errors = True
         # 1. check there are no unknown visa types
         if len(self.sponsors_df[~self.sponsors_df['tier_subtype'].isin(self.tier_subtypes)]) > 0:
-            self.prob_error = True
+            no_errors = False
             print(f'WARNING: '
                   f'{self.sponsors_df.tier_subtype[~self.sponsors_df.tier_subtype.isin(self.tier_subtypes)].values}'
                   f' are not in the tier subtypes list')
-
-        # 2. check new sponsor list does not differ much (<20%?) from the previous one.
-        def prev_data():
-            # find all sponsors file and return the path to the last one
-            files = glob('sponsors/tier*.csv')
-            files = sorted(files)
-            if self.csv_data_file in files:
-                files.remove(self.csv_data_file)
-            if files:
-                return files[-1]
-        last_data = prev_data()
-        if not last_data:
-            return
-        last_sponsors = SponsorsData(last_data, encoding=self.file_encoding,
-                                     to_csv=False, to_db=False, check_errors=False)
-        if len(self.diff(last_sponsors).index) >= 0.4 * len(self.sponsors_df.index):
-            print(f'New sponsor list differs too much (>20%) from the previous one')
-            self.prob_error = True
+        return no_errors
 
     def fix_missed_tier_type(self):
         def correct_tier_type(x):
@@ -150,7 +101,7 @@ class SponsorsData:
             tier_type = [i[0] for i in self.tier_type_subtypes if i[1] == x['tier_subtype']]
             if tier_type:
                 x['tier_type'] = tier_type[0]
-                # print(f'tier visa subtype was corrected for :\n{x}')
+                print(f'Missed data - tier visa type was corrected for :\n{x}')
             else:
                 print(f"Unknown tier visa subtype: {x}")
                 self.prob_error = True
@@ -228,14 +179,14 @@ class SponsorsData:
                     if ('tier_type' not in tier_options[-1]) and (not tag.find('b')):
                         continue
                 else:
-                    print(f'Unknown Tier type: {tag.text}')
+                    print(f'Tier type expected. {tag.text} got. Missed data probable.')
                     tag = next_info_tag(tag)
                 tier_options.append({})
             for tier_option in tier_options[:-1]:
                 for field in tier_fields:
                     if field not in tier_option:
                         print(f'{tier_fields.difference(set(tier_option.keys()))} '
-                              f'unknown for the organization: {curr_row}')
+                              f'missed for the organization: {curr_row}')
                         break
                 sponsors.append({**curr_row, **tier_option})
 
@@ -243,22 +194,75 @@ class SponsorsData:
 
         columns = ['name', 'city', 'county', 'tier_type', 'tier_rating', 'tier_subtype']
         sponsors = pd.DataFrame(sponsors, columns=columns)
+        print('XML sponsors file is successfully converted to DF')
         return sponsors
 
 
+def find_sponsors_url(source_url):
+    r = requests.get(source_url)
+    soup = BeautifulSoup(r.content, features="lxml")
+    sponsors_link = soup.find('section', {'id': 'documents'}). \
+        find('div', {'class': 'attachment-details'}).find('a').get('href')
+    return sponsors_link
+
+
+def get_sponsors_parsed(pdf_url):
+    doc_date = re.search('\/20(\d{2}-\d{2}-\d{2})', pdf_url).groups()[0].replace('-', '')
+    file_path_no_ext = f'{rel_data_path}/{file_name_prefix}_{doc_date}'
+
+    # no need to do anything if this data has already been downloaded (called from __main__)
+    if Path(f'{file_path_no_ext}.csv').exists():
+        print(f'{Path(file_path_no_ext).name} has already been processed')
+        sys.exit(0)
+
+    with open(f'{file_path_no_ext}.pdf', 'wb') as f:
+        f.write(requests.get(pdf_url).content)
+    print('PDF sponsors file downloaded')
+    with open(f'{file_path_no_ext}.pdf', 'rb') as in_f,\
+         open(f'{file_path_no_ext}.xml', 'w', encoding='utf-8') as out_f:
+        # options: -q : quiet, -i : ignore images
+        cmd = f'pdftohtml -xml -enc UTF-8 -q -i {in_f.name} {out_f.name}'
+        os.system(cmd)
+        print('PDF sponsors file successfully converted to XML')
+    return f'{file_path_no_ext}.xml'
+
+
 def download():
-    gov_url = r'https://www.gov.uk/government/publications/register-of-licensed-sponsors-workers'
     sponsors_url = find_sponsors_url(gov_url)
-    file_path, file_date = get_sponsors_parsed(sponsors_url)
-    # if os.path.isfile(file_path.replace('.xml', '.csv')):
-    #     print(f'Data for date {file_date} already loaded.')
-    return file_path, file_date
+    return get_sponsors_parsed(sponsors_url)
+
+
+def last_data_files(n=2, ext='csv'):
+    # find all sponsors file and return the path to the freshest one
+    files = glob(f'{rel_data_path}/tier*.{ext}')
+    files = sorted(files)
+    return files[-n:]
+
+
+def small_diff(sd):
+    # check new sponsor list does not differ much (<20%?) from the previous one
+    # take two last csv files
+    sponsor_files = last_data_files()
+    # it's expected that data we validate has (just) been saved in csv with the latest date in the file name
+    assert re.sub('^./', '', sponsor_files[-1]) == re.sub('^./', '', sd.csv_data_file)
+    # if no previous data found
+    if len(sponsor_files) < 2:
+        print('No prev data found to compare for difference')
+        return True
+    prev_sd = SponsorsData(sponsor_files[0])
+    if len(sd.diff(prev_sd).index) >= 0.2 * (len(sd.sponsors_df.index) + len(prev_sd.sponsors_df.index)) / 2:
+        print('New sponsor list differs too much (>20%) from the previous one')
+        return False
+    print(f'New sponsor list does not differ much from the previous one')
+    return True
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        file_path, file_date = download()
+        file_path = download()
     else:
         file_path = sys.argv[1]
-        file_date = re.search('_(\d{6})\.', file_path).groups()[0]
-    sd = SponsorsData(file_path, date=file_date, to_db=True, check_errors=True, to_db_if_error=False, db_settings=params)
+    sd = SponsorsData(file_path)
+    # if no new/suspicious data and it hasn't been changed too much from the previous one
+    if sd.validate() and small_diff(sd):
+        sd.insert_into_db(params)
